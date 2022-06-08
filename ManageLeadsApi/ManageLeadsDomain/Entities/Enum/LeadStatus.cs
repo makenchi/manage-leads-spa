b77a3eaf27e2e8ff5fbefda4b6054fac775e880a
makenchi <lucas.santos9322@gmail.com>
2022-06-07 21:35:03 -0300

    feat: Criando modelo DDD e criando classes de entidades

---
+++ ManageLeadsApi/ManageLeadsDomain/Entities/Enum/LeadStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageLeadsDomain.Entities.Enum
+{
+    public enum LeadStatus
+    {
+        Invited = 0,
+        Accepted = 1,
+        Declined = 2
+    }
+}
---
+++ ManageLeadsApi/ManageLeadsDomain/Entities/Lead.cs
@@ -0,0 +1,21 @@
+﻿using ManageLeadsDomain.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageLeadsDomain.Entities
+{
+    public class Lead : Base
+    {
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Suburb { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public LeadStatus Status { get; set; }
+    }
+}
