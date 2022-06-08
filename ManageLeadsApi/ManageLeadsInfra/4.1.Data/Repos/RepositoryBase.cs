a43016d8decff6af6fe6d291f0f477ae5e38b1b0
makenchi <lucas.santos9322@gmail.com>
2022-06-07 23:52:16 -0300

    feat: Implementando repos

---
+++ ManageLeadsApi/ManageLeadsInfra/4.1.Data/Repos/RepositoryBase.cs
@@ -0,0 +1,72 @@
+﻿using ManageLeadsDomainCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageLeadsInfra.Data.Repos
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly SqlContext _sqlContext;
+
+        public RepositoryBase(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        public void Add(TEntity obj)
+        {
+            try
+            {
+                _sqlContext.Set<TEntity>().Add(obj);
+                _sqlContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void Delete(TEntity obj)
+        {
+            try
+            {
+                _sqlContext.Set<TEntity>().Remove(obj);
+                _sqlContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _sqlContext.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _sqlContext.Set<TEntity>().Find(id);
+        }
+
+        public void Update(TEntity obj)
+        {
+            try
+            {
+                _sqlContext.Entry(obj).State = EntityState.Modified;
+                _sqlContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }  
+}
---
+++ ManageLeadsApi/ManageLeadsInfra/4.1.Data/Repos/RepositoryLead.cs
@@ -0,0 +1,19 @@
+﻿using ManageLeadsDomain.Entities;
+using ManageLeadsDomainCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageLeadsInfra.Data.Repos
+{
+    public class RepositoryLead : RepositoryBase<Lead>, IRepositoryLead
+    {
+        private readonly SqlContext _sqlContext;
+        public RepositoryLead(SqlContext sqlContext) : base(sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+    }
+}
