f6494295db19d836967aafa081ab6cf88ff767f8
makenchi <lucas.santos9322@gmail.com>
2022-06-07 22:32:28 -0300

    feat: Criando sql context

---
+++ ManageLeadsApi/ManageLeadsInfra/4.1.Data/SqlContext.cs
@@ -0,0 +1,19 @@
+﻿using ManageLeadsDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageLeadsInfra.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+        public DbSet<Lead> Leads { get; set; }
+    }
+}
