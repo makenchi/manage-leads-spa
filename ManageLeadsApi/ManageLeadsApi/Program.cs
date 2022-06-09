dfd60749d9914bd45338b108f6c7da57747f0d91
makenchi <lucas.santos9322@gmail.com>
2022-06-09 00:23:03 -0300

    feat: baixando dotnet ef para o projeto e adicionando o dbcontext ao program.cs

--- ManageLeadsApi/ManageLeadsApi/Program.cs
+++ ManageLeadsApi/ManageLeadsApi/Program.cs
@@ -1,9 +1,15 @@
+using ManageLeadsInfra.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SqlContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
