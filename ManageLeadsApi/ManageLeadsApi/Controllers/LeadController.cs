d3786de75c1cfb70bf474c8e38c5fe985ca161d2
makenchi <lucas.santos9322@gmail.com>
2022-06-08 21:43:04 -0300

    feat: implementando a camada de services e criando controller

---
+++ ManageLeadsApi/ManageLeadsApi/Controllers/LeadController.cs
@@ -0,0 +1,26 @@
+﻿using ManageLeadsApp.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageLeadsApi.Controllers
+{
+    [Route("lead/")]
+    [ApiController]
+    public class LeadController : ControllerBase
+    {
+        private readonly IApplicationServiceLead _applicationServiceLead;
+
+        public LeadController(IApplicationServiceLead applicationServiceLead)
+        {
+            _applicationServiceLead = applicationServiceLead;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_applicationServiceLead.GetAll());
+        }
+
+
+    }
+}
--- ManageLeadsApi/ManageLeadsApi/Program.cs
+++ ManageLeadsApi/ManageLeadsApi/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Manage Leads API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,7 +17,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Manage Leads API");
+    });
 }
 
 app.UseHttpsRedirection();
